afd76310987b293146b90d415213b16976c597f8
stere8 <twizoraph4247@gmail.com>
2024-04-24 13:30:40 +0200

    *Clean up

---
+++ StockIT/Pages/AboutMe.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace StockIT.Pages
+{
+    public class AboutMeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
